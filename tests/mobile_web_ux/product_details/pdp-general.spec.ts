0ccfe815810ac9b146db3bdbbf600416717617c3
KelMHunt <kel.m.hunt@gmail.com>
2026-09-09 13:16:14 -0500

    M-3: complete pdp general tests

--- tests/mobile_web_ux/product_details/pdp-general.spec.ts
+++ tests/mobile_web_ux/product_details/pdp-general.spec.ts
@@ -33,18 +33,51 @@ test.describe('General Product Detail Page Tests', ()=> {
         const ratingNumber = await productPage?.getRatingNumber()
         const status = await productPage?.getStatus()
         const productId = await productPage?.getProductId()
+        const price = await productPage?.getProductPrice()
 
         //debug
-        // console.log(image, title, starRatingCount, ratingNumber, status, productId)
+        // console.log(image, title, starRatingCount, ratingNumber, status, productId, price)
 
         expect(image).toEqual(title)
         expect(status?.toLowerCase()).toContain(data.productDetails.status)
         expect(productId).toContain(data.productDetails.idText)
+        expect(price).toEqual(72999.00)
 
         if(starRatingCount){
             expect(ratingNumber).toBeDefined()
             expect(parseFloat(ratingNumber!)).toBeGreaterThan(0)
         }
+    })
+
+    test('Verify description content is displayed when user selects description link CFS-326', async()=> {
+        const descriptionText = await productPage?.getDescriptionContent()
+        expect(descriptionText).toContain(data.products.discounted)
+    })
+
+    test('Verify specification content is displayed when user selects specifications link CFS-327', async()=> {
+        const specsContent = await productPage?.getSpecsContent()
+        expect(specsContent).toContain(data.productDetails.specs)
+    })
+
+    test('Verify clicking add to cart button on product page adds item to cart CFS-328', async()=> {
+        const cart = await productPage?.addToCart()
+        const qty = await cart?.getCartBadge()
+        expect(qty).toEqual(1)
+    })
+
+    test('Verify user can increase item quantity in cart from product page after adding item to cart CFS-329', async()=> {
+        const cart = await productPage?.addToCart(2)
+        const qty = await cart?.getCartBadge()
+        expect(qty).toEqual(2)
+    })
+
+    test('Verify user can decrease item quantity in cart from product page after adding item to cart CFS-332', async()=> {
+        const cart = await productPage?.addToCart(3)
+        await productPage?.decreaseQuantity(2)
+        await page.waitForTimeout(5000)
+        await base.openCart()
+        const qty = await cart?.getCartQuantity()
+        expect(qty).toEqual(1)
     })
 })
 
